bee57a9e5e0b47236cd69c5ff2f27cf88d9586e7
tulefat <tolefat1989@gmail.com>
2026-02-07 12:52:43 +0300

    load stock in analysis and logic for calculation required for the analysis page

--- src/pages/analysis/Analysis.jsx
+++ src/pages/analysis/Analysis.jsx
@@ -7,10 +7,45 @@ import { CreateAnalysis, GetAnalysisByUser, UpdateAnalysis } from "../../service
 
 const Analysis = ({ user }) => {
     const navigate = useNavigate()
-    const {symbol} = useParams
+    const {symbol} = useParams()
 
 
     const [stock,setStock] =useState(null)
     const [analysis,setAnalysis] = useState(null)
     const [capexFactor, setCapexFactor] = useState(0.5)
 }
+
+const [ownerEarnings, setOwnerEarnings] = useState(0)
+const [fairValue10cap,setFairValue10Cap] = useState(0)
+const [percentDifferent,setPercentDifferent] = useState(0)
+const [status,setStatus] = useState("fair")
+
+const calculationStatus = (percentDifferent) => {
+  if (percentDifferent>10) return "undervalued"
+  if (percentDifferent) return "overvalued"
+  else return "fair"
+}
+
+useEffect (()=>{
+  const displayStock = async () => {
+    try {
+      const result = await GetStocks().find((ticker)=> (ticker.symbol).toUpperCase())
+      setStock(result)}
+      catch (error) {
+        console.log("Failed to load the stock",error)
+      }}
+
+      displayStock()},[symbol])
+
+useEffect(()=>{
+  const ownerEarnings = Number(stock.operatingCashFlow) - capexFactor * Math.abs(Number(stock.capitalExpenditure))
+
+  const fairValue10Cap = (ownerEarnings/Number(stock.outstandingShares)) * 10
+
+  const percentDifferent = ((fairValue10Cap - Number(stock.price))/Number(stock.price)) * 100
+
+  setOwnerEarnings(ownerEarnings)
+  setFairValue10Cap(fairValue10Cap)
+  setPercentDifferent(percentDifferent)
+  setStatus(calculationStatus(percentDifferent))
+}, [stock,capexFactor])
